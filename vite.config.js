638c9e4dca44d2b8fb0098ff3446c8d5fb0660d0
evoly <svetoch.svs@gmail.com>
2026-05-29 10:26:41 +0700

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import vituum from 'vituum';
-import pug from '@vituum/vite-plugin-pug'
+import pug from '@vituum/vite-plugin-pug';
+
+process.env.BROWSER = 'google-chrome';
 
 export default {
   plugins: [vituum(), pug({
     root: './src',
   })],
+  server: {
+    open: true,
+  }
 }

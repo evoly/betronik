b40c7001f66d16e73212afb38e7e8612bb4a81f1
evoly <svetoch.svs@gmail.com>
2026-05-29 10:28:56 +0700

    add responsive styles

--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
 import './style.css'
 
 
-const navLinks = document.querySelectorAll('.nav-list__item');
+const button = document.querySelector('.burger-menu');
+const nav = document.querySelector('nav');
+const navLinks = nav.querySelectorAll('.nav-list__item');
+const logo = document.querySelector('.logo');
+const body = document.querySelector('body');
 
-/*
 const classToggle = () => {
     button.classList.toggle('open');
     nav.classList.toggle('active');
     body.classList.toggle('overflow-hidden');
 };
-*/
-const menuHandler = () => {
 
-    [...navLinks].forEach((link) => link.addEventListener('click', (event) => {
+const menuHandler = () => {
+    button.addEventListener('click', () => {
+        classToggle();
+    });
+
+    [...navLinks, logo].forEach((link) => link.addEventListener('click', (event) => {
         if (event.currentTarget.classList.contains('active')) {
             event.preventDefault();
         }
